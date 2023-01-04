ba214de5ebe25b7e25ee2bafd83ea3aff21cda1b
GeorgeLitescu <101198624+GeorgeLitescu@users.noreply.github.com>
2023-01-04 14:37:13 +0200

    Serialization exercise for a basic database

---
+++ Serialization/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public string country { get; set; }
+
+        public IList<Person> GetAllPersons()
+        {
+            return new List<Person>()
+            {
+                new Person { name = "Mitica", age = 14, country = "Argentina" },
+                new Person { name = "Magdalena", age = 98, country = "Israel" },
+                new Person { name = "Ionut", age = 24, country = "Romania" }
+            };
+        }
+    }
+}
---
+++ Serialization/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlCreator creator = new XmlCreator();
+            creator.DefaultXml();
+            creator.AddPerson("George", 21, "Romania");
+            creator.AddPerson("Miguel", 32, "Mexic");
+            creator.CreateXml();
+        }
+    }
+
+    internal class XmlCreator
+    {
+        Person person = new Person();
+        IList<Person> persons = new List<Person>();
+        String path = Directory.GetCurrentDirectory() + @"\people.xml";
+
+        public void CreateXml()
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(List<Person>));
+
+            using (FileStream stream = File.Create(path))
+            {
+                xs.Serialize(stream, persons);
+            }
+        }
+
+        public void AddPerson(string name, int age, string country)
+        {
+            persons.Add(new Person
+            {
+                name = name,
+                age = age,
+                country = country
+            });
+
+            //CreateXml();
+        }
+
+        public void DefaultXml()
+        {
+            persons = person.GetAllPersons();
+
+            //CreateXml();
+        }
+
+    }
+}
